d125fea5cd91f59381fe0346f1e7f2731f261410
Taylor Jadin <jadin.taylor@gmail.com>
2023-11-10 08:47:45 -0600

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# Script designed to be scheduled in cron:
+# @daily /root/kerkoapp/sync.sh > /root/kerkoapp/sync.log 2>&1
 
 cd /root/kerkoapp/ && docker-compose exec kerkoapp flask kerko sync
